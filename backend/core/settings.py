812dda19cfe56e6b4cf5dff8067d0397cafe9a32
Claudio D'Onofrio <claudio2000@hotmail.it>
2025-05-17 14:36:51 +0200

    Allowed Hosts backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -66,7 +66,8 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://robust-tranquility-production.up.railway.app"
 ]
 
 ROOT_URLCONF = 'core.urls'
